2e6cf261729a61907a245d68ef55a02fc64cd0f4
axrati <axmelluzzo@gmail.com>
2021-12-03 23:19:28 -0500

    Added Raleway and Kanit dependency loads, applied Raleway globally

---
+++ podax/src/components/Head_frame.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Head_frame.css'
+
+function Head_frame() {
+    return (
+        <div class="header_frame">
+            <div class="header_minimize"> - </div>
+            <div class="header_exit"> x </div>
+        </div>
+    )
+}
+
+export default Head_frame
